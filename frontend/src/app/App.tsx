027b6a712f9800bb30e954bec01430bcb5304a55
nikolai.b <nbardzinkevichh@gmail.com>
2025-02-20 13:11:29 +0800

    fix: rollbar error notification connection

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -9,21 +9,42 @@ import Chat from '../pages/Chat.tsx';
 import AuthForm from '../feauters/Login/Login.tsx';
 import NotFound from '../pages/NotFound.tsx';
 
+import {ErrorBoundary, Provider} from '@rollbar/react';
 
+const rollbarAccessToken = process.env.ROLLBAR_ACCESS_TOKEN_W;
+
+const rollbarConfig = {
+  accessToken: rollbarAccessToken,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true
+      },
+    },
+  },
+};
 
 function App() {
   const currentUser = useSelector(selectCurrentUser);
   const isLoggedIn = currentUser === null ? false : true;
 
   return (
-    <Routes>
-      <Route path={routes.notFound} element={<NotFound />} />
-      <Route path={routes.home} element=
-        { isLoggedIn ? <Chat /> : <Navigate to="/login" />}
-      />
-      <Route path={routes.login} element={<AuthForm />} />
-      <Route path={routes.signup} element={<Signup />} />
-    </Routes>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Routes>
+          <Route path={routes.notFound} element={<NotFound />} />
+          <Route path={routes.home} element=
+            { isLoggedIn ? <Chat /> : <Navigate to="/login" />}
+          />
+          <Route path={routes.login} element={<AuthForm />} />
+          <Route path={routes.signup} element={<Signup />} />
+        </Routes>
+      </ErrorBoundary>
+    </Provider>
+
   )
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,35 +11,14 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import { Provider as RollbarProvider } from '@rollbar/react';
-
-const rollbarAccessToken = process.env.ROLLBAR_ACCESS_TOKEN_R;
-
-const rollbarConfig = {
-  accessToken: rollbarAccessToken,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-  payload: {
-    client: {
-      javascript: {
-        code_version: '1.0.0',
-        source_map_enabled: true
-      },
-    },
-  },
-};
-
 console.log('i18n initialization: ', i18n);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RollbarProvider config={rollbarConfig}>
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
-    </RollbarProvider>
-
   </StrictMode>,
 );  
