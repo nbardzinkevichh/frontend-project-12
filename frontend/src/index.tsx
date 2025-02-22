d6250446fe9b6b86ed04894f35de0b2db251bb8a
nikolai.b <nbardzinkevichh@gmail.com>
2025-02-23 06:16:37 +0800

    fix: change package.json scripts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,15 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import i18n from './i18n/i18n';
 
 import App from './app/App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import i18n from './i18n/i18n';
 
 console.log('i18n initialization: ', i18n);
 
