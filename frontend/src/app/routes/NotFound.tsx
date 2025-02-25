4705931120bd5df8267f198d66bba5ac4a9dcbbc
nikolai.b <nbardzinkevichh@gmail.com>
2025-02-25 20:14:03 +0800

    refactor: not found page

--- frontend/src/app/routes/NotFound.tsx
+++ frontend/src/app/routes/NotFound.tsx
@@ -1,12 +1,18 @@
 import {useTranslation} from "react-i18next";
+import Header from "../../components/Header.tsx";
 
 export default function NotFound() {
   const { t } = useTranslation('pages');
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1>{t('notFound.notFoundError')}</h1>
-      <p>{t('notFound.notFoundDescription')} <a href="/">{t('notFound.notFoundLink')}</a></p>
-    </div>
+    <>
+      <Header status='loggedOut' />
+
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+        <h1>{t('notFound.notFoundError')}</h1>
+        <p>{t('notFound.notFoundDescription')} <a href="/">{t('notFound.notFoundLink')}</a></p>
+      </div>
+    </>
+
   )
 };
